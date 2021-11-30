c7527708f04eddfa16e1dce8bfb44e86e6a5c0db
Raúl Angel Jiménez <raulangelj@gmail.com>
2021-11-30 16:01:50 -0600

    Start design for sign in

--- src/components/signIn.jsx
+++ src/components/signIn.jsx
@@ -1,12 +1,28 @@
-/* eslint-disable no-unused-vars */
 import React from 'react'
+import { Helmet } from 'react-helmet'
+import Btn from '../elements/btn'
+import { Form, Input } from '../elements/formItems'
+import { Header, HeaderContainer, Title } from '../elements/header'
 
-const SignIn = () => {
-  const a = 0
-
-  return (
-    <h1>Sign In</h1>
-  )
-}
+const SignIn = () => (
+  <>
+    <Helmet>
+      <title>Sign in</title>
+    </Helmet>
+    <Header>
+      <HeaderContainer>
+        <Title>Sign in</Title>
+        <div>
+          <Btn to="/log-in">Log in</Btn>
+        </div>
+      </HeaderContainer>
+    </Header>
+    <Form>
+      <Input type="email" name="email" placeholder="Email" />
+      <Input type="password" name="password" placeholder="Password" />
+      <Input type="password" name="password2" placeholder="Confirm password" />
+    </Form>
+  </>
+)
 
 export default SignIn
